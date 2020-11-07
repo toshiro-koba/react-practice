945f0d81ae1ca62383c682611b6333944276622a
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-07 15:33:07 +0900

    first-comit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const logo = '/images/logo.png';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>manta Kawaii</b>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.forte2020.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Forte
         </a>
       </header>
     </div>
